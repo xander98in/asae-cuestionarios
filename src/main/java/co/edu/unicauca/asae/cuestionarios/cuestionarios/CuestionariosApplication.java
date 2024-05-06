fefc3b75d64c11c9c33d408841ab5f5a5ff58d78
xander98in <alexander98in@gmail.com>
2024-05-06 15:32:28 -0500

    Creación del proyecto

---
+++ src/main/java/co/edu/unicauca/asae/cuestionarios/cuestionarios/CuestionariosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.cuestionarios.cuestionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuestionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuestionariosApplication.class, args);
+	}
+
+}
