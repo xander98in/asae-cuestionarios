d49504c3973db7c245c0f89d9fc07be22865d257
Juan-MZ <juanjosz@unicauca.edu.co>
2024-05-08 08:40:02 -0500

    Manejo de excepciones para la salida

---
+++ src/main/java/co/edu/unicauca/asae/cuestionarios/cuestionarios/infraestructura/output/controladorExcepciones/excepcionesPropias/GestionClientesRuntimeException.java
@@ -0,0 +1,16 @@
+package co.edu.unicauca.asae.cuestionarios.cuestionarios.infraestructura.output.controladorExcepciones.excepcionesPropias;
+
+import co.edu.unicauca.asae.cuestionarios.cuestionarios.infraestructura.output.controladorExcepciones.estructuraExcepciones.CodigoError;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public abstract class GestionClientesRuntimeException extends RuntimeException {
+
+  protected CodigoError codigoError;
+
+  public abstract String formatException();
+}
