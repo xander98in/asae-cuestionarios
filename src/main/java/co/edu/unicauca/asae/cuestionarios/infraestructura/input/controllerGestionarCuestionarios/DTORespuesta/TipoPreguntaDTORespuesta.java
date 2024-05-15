ac92a3dbf8701bb28de6f74cf669f19c6e706900
Juan-MZ <juanjosz@unicauca.edu.co>
2024-05-15 08:38:45 -0500

    import added

--- src/main/java/co/edu/unicauca/asae/cuestionarios/infraestructura/input/controllerGestionarCuestionarios/DTORespuesta/TipoPreguntaDTORespuesta.java
+++ src/main/java/co/edu/unicauca/asae/cuestionarios/infraestructura/input/controllerGestionarCuestionarios/DTORespuesta/TipoPreguntaDTORespuesta.java
@@ -10,7 +10,5 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TipoPreguntaDTORespuesta {
-    
-    private String tipoPregunta;
-
+    private Integer idTipoPregunta;
 }
--- src/main/java/co/edu/unicauca/asae/cuestionarios/infraestructura/input/controllerGestionarCuestionarios/mappers/CuestionarioMapperInfraestructuraDominio.java
+++ src/main/java/co/edu/unicauca/asae/cuestionarios/infraestructura/input/controllerGestionarCuestionarios/mappers/CuestionarioMapperInfraestructuraDominio.java
@@ -5,14 +5,21 @@ import java.util.List;
 import org.mapstruct.Mapper;
 
 import co.edu.unicauca.asae.cuestionarios.dominio.modelos.Cuestionario;
+import co.edu.unicauca.asae.cuestionarios.dominio.modelos.TipoPregunta;
 import co.edu.unicauca.asae.cuestionarios.infraestructura.input.controllerGestionarCuestionarios.DTOPeticion.CuestionarioDTOPeticion;
+import co.edu.unicauca.asae.cuestionarios.infraestructura.input.controllerGestionarCuestionarios.DTOPeticion.TipoPreguntaDTOPeticion;
 import co.edu.unicauca.asae.cuestionarios.infraestructura.input.controllerGestionarCuestionarios.DTORespuesta.CuestionarioDTORespuesta;
+import co.edu.unicauca.asae.cuestionarios.infraestructura.input.controllerGestionarCuestionarios.DTORespuesta.TipoPreguntaDTORespuesta;
 
 @Mapper(componentModel = "spring")
 public interface CuestionarioMapperInfraestructuraDominio {
     Cuestionario mappearDePeticionACuestionario(CuestionarioDTOPeticion peticion);
 
     CuestionarioDTORespuesta mappearDeCuestionarioARespuesta(Cuestionario objCuestionario);
+
+    TipoPreguntaDTORespuesta mappearTipoPreguntaARespuesta(TipoPregunta tipoPregunta);
+
+    TipoPregunta mappearDePeticionATipoPregunta(TipoPreguntaDTOPeticion tipoPreguntaDTOPeticion);
 
     List<CuestionarioDTORespuesta> mappearDeCuestionariosARespuesta(List<Cuestionario> Cuestionarios);
 }
